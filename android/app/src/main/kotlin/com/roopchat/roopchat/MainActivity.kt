6d15da3870d90cb392a84b8717b5d5eda8152d67
rooptestdev <92123921+rooptestdev@users.noreply.github.com>
2021-11-24 23:03:29 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/roopchat/roopchat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.roopchat.roopchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/message_dao.dart
@@ -0,0 +1,15 @@
+import 'message.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class MessageDao {
+  final DatabaseReference _messagesRef =
+      FirebaseDatabase.instance.reference().child('messages');
+
+  void saveMessage(Message message) {
+    _messagesRef.push().set(message.toJson());
+  }
+
+  Query getMessageQuery() {
+    return _messagesRef;
+  }
+}
